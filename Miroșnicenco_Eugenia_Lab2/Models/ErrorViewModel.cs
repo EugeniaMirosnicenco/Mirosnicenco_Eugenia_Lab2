e264de52ebfcb0229bf4b7907c1bd957341f1d1a
EugeniaMirosnicenco <miroshnicenco36@gmail.com>
2025-12-16 17:53:43 +0200

    Add project files.

---
+++ Miroșnicenco_Eugenia_Lab2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Miroșnicenco_Eugenia_Lab2.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
